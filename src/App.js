09f4f64954d2ca1cbe36fd041da8e678fb39d6e3
David Perelman <dperelman3@gmail.com>
2022-12-11 19:50:56 +0200

    setup Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

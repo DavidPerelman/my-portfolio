c37e7009f79d1a89873221d3c08e33090e4d7be9
David Perelman <dperelman3@gmail.com>
2023-02-14 12:14:09 +0200

    add My Cool Store

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,6 +2,14 @@ import tipCalculatorAppWeb from '../assets/tip-calculator-app-1.png';
 import tipCalculatorAppMob from '../assets/tip-calculator-app-mobile.png';
 import randomQuoteMachineWeb from '../assets/random-quote-machine-1.png';
 import randomQuoteMachineMob from '../assets/random-quote-machine-mobile.jpeg';
+import myCoolStoreWeb from '../assets/my-cool-store.png';
+import myCoolStoreWeb1 from '../assets/my-cool-store (1).png';
+import myCoolStoreWeb2 from '../assets/my-cool-store (2).png';
+import myCoolStoreWeb3 from '../assets/my-cool-store (3).png';
+import myCoolStoreWeb4 from '../assets/my-cool-store (4).png';
+import myCoolStoreMob from '../assets/my-cool-store-mobile.jpeg';
+import myCoolStoreMob1 from '../assets/my-cool-store-mobile (1).jpeg';
+import myCoolStoreMob2 from '../assets/my-cool-store-mobile (1).jpeg';
 
 export const ProjectList = [
   {
@@ -17,5 +25,21 @@ export const ProjectList = [
     skills: 'React',
     github: 'https://github.com/DavidPerelman/random-quote-machine',
     webLink: 'https://david-random-quote-machine.netlify.app/',
+  },
+  {
+    name: 'My Cool Store',
+    image: [
+      myCoolStoreWeb,
+      myCoolStoreWeb1,
+      myCoolStoreWeb2,
+      myCoolStoreWeb3,
+      myCoolStoreWeb4,
+      myCoolStoreMob,
+      myCoolStoreMob1,
+      myCoolStoreMob2,
+    ],
+    skills: 'Next.js',
+    github: 'https://github.com/DavidPerelman/my-cool-store-next-js',
+    webLink: 'https://my-cool-store-next-js.netlify.app/',
   },
 ];

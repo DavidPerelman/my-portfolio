38f3ec08dbdc1a0de6ea0d61aba5cade20f49b00
David Perelman <dperelman3@gmail.com>
2023-02-15 12:28:06 +0200

    deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import { Email, GitHub, LinkedIn } from '@mui/icons-material';
 import '../styles/Home.css';
 import profileImage from '../assets/profile-img.jpg';
 
 const Home = () => {
-  const linkedInLink = 'https://www.linkedin.com/in/davidperelman3/';
-  const githubLink = 'https://github.com/DavidPerelman';
-  const facebookLink = 'https://www.facebook.com/david.perelman.9/';
-
   return (
     <div className='home'>
       <div className='about'>
-        <img className='profile-img' src={profileImage} />
+        <img className='profile-img' src={profileImage} alt='profile-img' />
         <h2>דוד פרלמן</h2>
         <div className='propmt'>
           <h3>Full Stack Web Developer</h3>
-          {/* <div className='socialMedia'>
-            <a href={linkedInLink}>
-              <LinkedIn />
-            </a>
-            <Email />
-            <a href={githubLink}>
-              <GitHub />
-            </a>
-          </div> */}
         </div>
       </div>
       <div className='skills'>
